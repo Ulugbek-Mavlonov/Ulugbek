bc2f92f99e0fd7a4d9f3c33a3ba77e901e255abb
Ulugbek <ulugbekmavlonov@gmail.com>
2023-03-03 22:17:36 +0500

    hw decorators

---
+++ decorator.py
@@ -0,0 +1,38 @@
+# 1
+# def add_one(func):
+#     add_one = func
+#     return add_one
+#
+#
+# def plus_one(number):
+#     return number + 1
+#
+#
+# print(add_one(plus_one(5)))
+# 2
+# def uppercase_decorator(func):
+#     return str.upper(func)
+#
+#
+# def say_hi():
+#     return "hallo there"
+#
+#
+# print(uppercase_decorator(say_hi()))
+
+# def add_number(func):
+#     def num(*args):
+#         return "+998 " + func(*args)
+#
+#     return num
+#
+#
+# @add_number
+# def number(arg):
+#     return arg
+#
+#
+# print(number("939074159"))
+
+def add_phone(func):
+
